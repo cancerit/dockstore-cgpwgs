470587760f599edd2dea7f7f0f3d70ebbecd97fa
Keiran Raine <kr2@sanger.ac.uk>
2017-01-16 19:08:27 +0000

    Missing dependancy

--- build/opt-build.sh
+++ build/opt-build.sh
@@ -15,6 +15,15 @@ cd /tmp/downloads/distro
 cd /tmp/downloads
 rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
 
+# cgpNgsQc
+curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/cgpNgsQc/archive/v1.3.0.zip
+mkdir /tmp/downloads/distro
+bsdtar -C /tmp/downloads/distro --strip-components 1 -xf distro.zip
+cd /tmp/downloads/distro
+./setup.sh $OPT
+cd /tmp/downloads
+rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
+
 # ascatNgs
 curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/ascatNgs/archive/v4.0.0.zip
 mkdir /tmp/downloads/distro
