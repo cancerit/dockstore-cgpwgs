7f33d210dadf77c044f8f36e05ff7da0d9a00e3a
sdentro <sd11@sanger.ac.uk>
2018-11-17 17:51:13 +0000

    fix installation of copynumber dependency of brass

--- build/rlib-build.R
+++ build/rlib-build.R
@@ -36,4 +36,4 @@ ipak(c("ggplot2"))
 ipak(c("gridExtra"))
 ipak(c("gtools"))
 
-install_github("sb43/copynumber", ref="f1688edc154f1a0e3aacf7781090afe02882f623")
+install_github("Irrationone/copynumber", ref="87d2663fe6b11c03cf6006b4ee9ed70450eacb5a")
